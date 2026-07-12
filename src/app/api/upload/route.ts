27d445201fea0ee9a63ba93c637fa53683af77f8
benyigit - Yiğit Özcan <sucukluyum12@gmail.com>
2026-07-12 03:36:19 +0300

    Fix Vercel Blob overwrite error

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -17,6 +17,7 @@ export async function POST(request: Request): Promise<NextResponse> {
 
     const blob = await put(filename, request.body, {
       access: 'public',
+      addRandomSuffix: true,
     });
 
     return NextResponse.json(blob);
